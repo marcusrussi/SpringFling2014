201f13b425587b3d17e3b499dfccf02303ce0523
Marcus Russi <marcusrussi@gmail.com>
2014-04-17 06:05:33 -0400

    Added images, changed a lot of stuff

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,18 @@ router.get('/', function(req, res) {
   var data = {
     title: 'Spring Fling 2014',
     studentBands: [
-      {name: 'Student Band #1', type: 'student band', startTime: '3:00pm'},
-      {name: 'The Rain Brigade', type: 'student band', startTime: '3:45pm'},
-      {name: 'Nero, My Panda', type: 'student band', startTime: '4:30pm'},
+      {name: 'Student Band #1',type: 'student band', startTime: '3:00pm', image: 'images/placeholder.gif'},
+      {name: 'The Rain Brigade', type: 'student band', startTime: '3:45pm', image: 'images/therainbrigade.jpg'},
+      {name: 'Nero, My Panda', type: 'student band', startTime: '4:30pm', image: 'images/neromypanda.jpg'},
     ],
     artists: [
-      {name: 'Kyle Parsard aka Fusion', type: 'student dj', startTime: '5:20pm'},
-      {name: 'Nick Mars', type: 'student dj', startTime: '5:40pm'},
-      {name: 'Betty Who', type: 'headliner', startTime: '6:00pm'},
-      {name: 'La Vie en Twerk', type: 'student dj', startTime: '7:05pm'},
-      {name: 'Chance the Rapper', type: 'headliner', startTime: '7:30pm'},
-      {name: 'Thomas Rokholt', type: 'student dj', startTime: '8:35pm'},
-      {name: 'Diplo', type: 'headliner', startTime: '9:00pm'}
+      {name: 'Kyle Parsard aka Fusion', type: 'student dj', startTime: '5:20pm', image: 'images/kyleparsard.jpg', student: true},
+      {name: 'Nick Mars', type: 'student dj', startTime: '5:40pm', student: true},
+      {name: 'Betty Who', type: 'headliner', startTime: '6:00pm', image: 'images/bettywho.jpg'},
+      {name: 'La Vie en Twerk', type: 'student dj', startTime: '7:05pm', image: 'images/lavieentwerk.jpg', student: true},
+      {name: 'Chance the Rapper', type: 'headliner', startTime: '7:30pm', image: 'images/chance.jpg'},
+      {name: 'Thomas Rokholt', type: 'student dj', startTime: '8:35pm', student: true},
+      {name: 'Diplo', type: 'headliner', startTime: '9:00pm', image: 'images/diplo.jpg'}
     ]
 
   };
